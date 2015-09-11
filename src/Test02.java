4ea506a8d5261075ddb555e831cb5d25dba61ccd
unknown <x15g004@PC505N015.chiba-fjb.ac.jp>
2015-09-11 11:51:43 +0900

    変更

--- src/Test02.java
+++ src/Test02.java
@@ -4,6 +4,8 @@ public class Test02 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 System.out.println("あああい");
+System.out.println("あああい");
+System.out.println("あああい");
 	}
 
 }
